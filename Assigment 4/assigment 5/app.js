5d17d1c4e94536a8ce443fba42ca67178aba57f7
Muhammad Ilyas <milyasather42401@gmail.con>
2025-02-22 17:03:20 +0500

    complete 1 to 7

---
+++ Assigment 4/assigment 5/app.js
@@ -0,0 +1,101 @@
+let a = 10;
+let b = 5;
+ let sum= 10 + 5;
+sub = 10 - 5;
+mul = 10 * 5;
+division = 10 / 5;
+modulus = 10 % 5;
+
+document.write("1)Sum of 10 and 5 is = " + sum)
+document.write("<br>");
+document.write("<br>");
+document.write("2)Subtraction of 10 and 5 is = " + sub)
+document.write("<br>");
+document.write("<br>");
+document.write("multiplication of 10 and 5 is = " + mul)
+document.write("<br>");
+document.write("<br>");
+document.write("Division of 10 and 5 is = " + division)
+document.write("<br>");
+document.write("<br>");
+document.write("Modulus of 10 and 5 is = " + modulus)
+document.write("<br>");
+document.write("<br>");
+
+let myName;
+    name = "Muhammad Ilyas"
+document.write("3)Value after variable declaration is: " + name)
+document.write("<br>");
+//initialize
+let number = 5;
+document.write("Initial value: " + number)
+number++;
+document.write("<br>");
+document.write(" Value after increment is: " + number)
+let add_number = 6 + 7
+document.write("<br>");
+document.write(" Value after addition: " + add_number)
+//decrement
+add_number--;
+document.write("<br>");
+document.write(" Value after decrement: " + add_number)
+let rem = 12 % 3;
+document.write("<br>");
+document.write("The Value of remainder is: " + rem)
+//movie tickets
+let movieTicket = 600;
+let fiveTickets = 600 * 5;
+document.write("<br>");
+document.write("Total Cost to buy 5 tickets to a movie is : " + fiveTickets + 'PKR')
+    //Table of 5;
+let x = 5;
+let y = 1;
+let z1 = 5*1
+let z2 = 5*2
+let z3 = 5*3
+let z4 = 5*4
+let z5 = 5*5
+let z6 = 5*6
+let z7 = 5*7
+let z8 = 5*8
+let z9 = 5*9
+let z10 = 5*10
+
+document.write("<br>");
+document.write(" Table of 5 " )
+document.write("<br>");
+document.write("5x1 = " + z1 )
+document.write("<br>");
+document.write("5x2 = " + z2 )
+document.write("<br>");
+document.write("5x3 = " + z3 )
+document.write("<br>");
+document.write("5x4 = " + z4 )
+document.write("<br>");
+document.write("5x5 = " + z5 )
+document.write("<br>");
+document.write("5x6 = " + z6 )
+document.write("<br>");
+document.write("5x7 = " + z7 )
+document.write("<br>");
+document.write("5x8 = " + z8 )
+document.write("<br>");
+document.write("5x9 = " + z9 )
+document.write("<br>");
+document.write("5x 10 = " + z10)
+//6. The Temperature Converter: °F = (°C x 9/5) + 32
+document.write("<br>");
+document.write("The Temperature Converter:")
+const Celsius = 25;
+const Fahrenheit = (25 * 9 / 5) + 32;
+document.write("<br>");
+document.write("25°C is ", +  Fahrenheit +"°F")
+// °C = (°F - 32)x 5/9
+
+const Fahren = 70;
+const Celsi = (70 - 32) * 5/9;
+document.write("<br>");
+document.write("70°F is " + Celsi)
+
+
+
